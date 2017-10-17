c2a6d4c9405a9427cdb64f822b203c36d57d28fc
Jason Dixon <jdixon@wetafx.co.nz>
2017-10-18 09:02:32 +1300

    simple monitor

--- activity.py
+++ activity.py
@@ -1,1 +1,30 @@
 # Keep track of activity in maya.
+import db
+import time
+import threading
+
+class Monitor(object):
+    """ Monitor status and periodically poll DB """
+    def __init__(s, path_to_db, software, user):
+        s.db = db.DB(path_to_db)
+        s.poll_interval = db.MINUTE * 5
+        s.last_active = time.time()
+        s.notes = ""
+        s.software = software
+        s.user = user
+        s.path = ""
+
+    def start(s):
+        """ Begin polling """
+        threading.Thread(target=s.poll).start()
+
+    def poll(s):
+        """ Update DB with activity """
+        while True:
+            last_active = (time.time() - s.last_active) <= s.poll_interval
+            s.db.poll(s.user, s.software, s.path, "active" if last_active else "idle", s.notes
+            time.sleep(s.poll_interval)
+
+    def checkin(s):
+        """ Check in to show activity with software """
+        s.last_active = time.time()
