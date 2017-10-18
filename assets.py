268a5c653d728274130192598479de1f9e622de2
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-19 06:58:01 +1300

    nice

--- assets.py
+++ assets.py
@@ -14,6 +14,7 @@ class Asset(object):
     def __init__(s):
         # Load our assets in
         s.assets = {}
+        s.vars = {}
         for root, dirs, files in os.walk(ASSET_ROOT):
             rel_root = os.path.relpath(root, ASSET_ROOT)
             for f in files:
@@ -23,14 +24,22 @@ class Asset(object):
     def compile(s, title="TITLE"):
         """ Build our page with all our assets combined """
         index = s.assets["index.html"]
+        s.vars["title"] = "MY TITLE!"
+
+        for m in reversed(tuple(re.finditer(r"{{(?P<action>\w+)\s+(?P<var>[\w_-]+)}}", index))):
+            act = m.group("action")
+            if act == "replace":
+                index = index[:m.start(0)] + s.vars.get(m.group("var"), "") + index[m.end(0):]
 
         for m in reversed(tuple(re.finditer(r"<\s*(?P<tag>\w+).+?(?P<quote>'|\")(?P<asset>[-_\w\s\./]+\.\w+)(?P=quote).+", index))):
             tag = m.group("tag")
             if tag == "link": # CSS sheet
-                index = index.replace(m.group(0), "<style media=\"screen\">\n{}</style>".format(s.assets[m.group("asset")]))
-            if tag == "script": # Javascript
-                index = index.replace(m.group(0), "<script type=\"text/javascript\">\n{}</script>".format(s.assets[m.group("asset")]))
-
+                repl = "<style media=\"screen\">\n{}</style>".format(s.assets[m.group("asset")])
+            elif tag == "script":
+                repl = "<script type=\"text/javascript\">\n{}</script>".format(s.assets[m.group("asset")])
+            else:
+                repl = m.group(0)
+            index = index[:m.start(0)] + repl + index[m.end(0):]
         return index
 
     def view(s):
@@ -41,4 +50,5 @@ class Asset(object):
 
 if __name__ == '__main__':
     a = Asset()
+    # print(a.compile())
     a.view()
