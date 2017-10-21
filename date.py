0798c727914ef6ff3060ce4beb210b6e87439d25
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-21 21:37:22 +1300

    gather week timestamps

--- date.py
+++ date.py
@@ -2,6 +2,8 @@
 from __future__ import print_function, division
 import time
 import datetime
+import calendar
+import collections
 
 # Helpers to work with times
 SECOND = 1.0
@@ -12,10 +14,23 @@ WEEK = DAY * 7
 WORK_WEEK = DAY * 5
 YEAR = DAY * 365
 
+def week_timestamp(start_day="monday"):
+    """ Output a week of timestamps """
+    today = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
+    weekday = today.weekday()
+    week = list(calendar.Calendar(getattr(calendar, start_day.upper())).iterweekdays())
+    pos = week.index(weekday)
+    week_data = (today - datetime.timedelta(days=pos - i) for i, d in enumerate(week))
+    return collections.OrderedDict([(calendar.day_name[a], (b.timestamp(), b.timestamp() + HOUR * 24)) for a, b in zip(week, week_data)])
+
 # Helpers for weeks
 mon_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 sun_week = mon_week[6:] + mon_week[:6]
 work_week = mon_week[:5]
+
+
+# import calendar
+# print([a for a in calendar.Calendar(calendar.SUNDAY).iterweekdays()])
 
 def current_week_mon():
     """ Week up to point, mon start """
@@ -61,4 +76,10 @@ def to_time(timestamp):
     minutes = (timestamp % HOUR) / MINUTE
     return "{}:{} Hours:Minutes".format(int(hours), str(int(minutes)).zfill(2))
 
-print(current_week_work())
+print(week_timestamp("wednesday"))
+# import datetime
+# import pprint
+#
+# today = datetime.date(2017, 10, 20)
+# dates = [today + datetime.timedelta(days=i) for i in range(0 - today.weekday(), 7 - today.weekday())]
+# pprint.pprint(dates)
