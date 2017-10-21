7e479aab86713709a615531be105088845b75bdb
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-21 21:40:18 +1300

    gather week timestamps

--- date.py
+++ date.py
@@ -23,45 +23,6 @@ def week_timestamp(start_day="monday"):
     week_data = (today - datetime.timedelta(days=pos - i) for i, d in enumerate(week))
     return collections.OrderedDict([(calendar.day_name[a], (b.timestamp(), b.timestamp() + HOUR * 24)) for a, b in zip(week, week_data)])
 
-# Helpers for weeks
-mon_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-sun_week = mon_week[6:] + mon_week[:6]
-work_week = mon_week[:5]
-
-
-# import calendar
-# print([a for a in calendar.Calendar(calendar.SUNDAY).iterweekdays()])
-
-def current_week_mon():
-    """ Week up to point, mon start """
-    return current_week(mon_week)
-
-def current_week_sun():
-    """ Week up to point, sun start """
-    return current_week(sun_week)
-
-def current_week_work():
-    """ Week up to point, work start """
-    try:
-        return current_week(work_week)
-    except ValueError:
-        return current_week(mon_week)
-
-def current_week(week):
-    """ Week up to this point """
-    curr = current_day()
-    pos = week.index(curr.strftime("%A"))
-    return [curr - datetime.timedelta(days=a) for a in range(pos + 1)][::-1]
-
-def current_day():
-    """ Get current day """
-    curr = current() # Current time.
-    return curr - datetime.timedelta(
-        hours=curr.hour,
-        minutes=curr.minute,
-        seconds=curr.second,
-        microseconds=curr.microsecond)
-
 def timestamp():
     """ Current time as float timestamp """
     return time.time()
@@ -76,10 +37,4 @@ def to_time(timestamp):
     minutes = (timestamp % HOUR) / MINUTE
     return "{}:{} Hours:Minutes".format(int(hours), str(int(minutes)).zfill(2))
 
-print(week_timestamp("wednesday"))
-# import datetime
-# import pprint
-#
-# today = datetime.date(2017, 10, 20)
-# dates = [today + datetime.timedelta(days=i) for i in range(0 - today.weekday(), 7 - today.weekday())]
-# pprint.pprint(dates)
+print(week_timestamp("sunday"))
